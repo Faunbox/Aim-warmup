56f1be9ebfdadc7b841cb8894f7869f426c5dca6
Filip <faunbox2@gmail.com>
2021-06-23 08:26:22 +0200

    init commit

---
+++ JS/Cell.js
@@ -0,0 +1,22 @@
+import { Ui } from "./Ui.js";
+
+export class Cell extends Ui {
+  constructor(x, y) {
+    super();
+    this.x = x;
+    this.y = y;
+    // this.isFake = false;
+    this.selector = `[data-x="${this.x}"][data-y="${this.y}"]`;
+    this.element = null;
+    this.isTarget = false;
+  }
+
+  createElement() {
+    const element = `<div class="cell border border--concave" data-cell data-x="${this.x}" data-y="${this.y}"></div>`;
+    return element;
+  }
+
+  addTarget() {
+    this.isTarget = true;
+  }
+}
